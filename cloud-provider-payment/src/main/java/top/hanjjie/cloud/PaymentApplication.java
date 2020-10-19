b18e0dc881dffc161c1c165f7a0bc05d7e3ac0bf
huanghanjie <huanghanjie@badousoft.com>
2020-10-19 15:34:25 +0800

    新建 支付模块 for Mac

---
+++ cloud-provider-payment/src/main/java/top/hanjjie/cloud/PaymentApplication.java
@@ -0,0 +1,16 @@
+package top.hanjjie.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 支付模块主启动类
+ */
+@SpringBootApplication
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+
+}
