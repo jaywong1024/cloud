0508a0a0fb9a97957cf417b847601b11cad16dd7
huanghanjie <huanghanjie@badousoft.com>
2020-10-23 14:12:33 +0800

    1. 将 payment 作为 eureka 客户端注册到服务 2. 修改配置文件使用父工程 .pom 统一管理
    for Mac

--- cloud-provider-payment/src/main/java/top/hanjjie/cloud/PaymentApplication.java
+++ cloud-provider-payment/src/main/java/top/hanjjie/cloud/PaymentApplication.java
@@ -3,11 +3,13 @@ package top.hanjjie.cloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 支付模块主启动类
  */
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan("top.hanjjie.cloud.dao")
 public class PaymentApplication {
 
