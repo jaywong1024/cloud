e3d14af6ea142325242d240d8756fbdc23e25b85
huanghanjie <huanghanjie@badousoft.com>
2020-10-24 17:23:19 +0800

    新建 order（订单） 模块 for Mac

---
+++ cloud-provider-order/src/main/java/top/hanjjie/cloud/OrderApplication.java
@@ -0,0 +1,20 @@
+package top.hanjjie.cloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 订单模块主启动类
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("top.hanjjie.cloud.dao")
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
---
+++ cloud-provider-order/src/main/java/top/hanjjie/cloud/controller/OrderController.java
@@ -0,0 +1,11 @@
+package top.hanjjie.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+}
