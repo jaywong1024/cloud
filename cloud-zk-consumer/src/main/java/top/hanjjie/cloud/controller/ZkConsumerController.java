a8f982686dcfedf3ce44914f3b5a66cbb2ed2b80
HANJJIE <1432345840@qq.com>
2020-12-24 21:44:48 +0800

    [完成] 使用 ZooKeeper 作为注册中心，进行服务和消费

---
+++ cloud-zk-consumer/src/main/java/top/hanjjie/cloud/controller/ZkConsumerController.java
@@ -0,0 +1,27 @@
+package top.hanjjie.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import top.hanjjie.cloud.utils.ResultBean;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class ZkConsumerController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${provider.zk}")
+    private String providerZK;
+
+    @GetMapping("/testZkConnecting")
+    private ResultBean<String> testZkConnecting() {
+        return restTemplate.getForObject(providerZK + "/testZkConnecting", ResultBean.class);
+    }
+
+}
