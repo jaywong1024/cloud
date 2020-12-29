2870375f84e11bb6b168513433274fd42a461de0
HANJJIE <1432345840@qq.com>
2020-12-29 22:58:01 +0800

    学习使用 restTemplate.exchange() 方法

--- cloud-consumer-client/src/main/java/top/hanjjie/cloud/controller/ClientController.java
+++ cloud-consumer-client/src/main/java/top/hanjjie/cloud/controller/ClientController.java
@@ -2,6 +2,9 @@ package top.hanjjie.cloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +23,12 @@ import java.util.Optional;
 @Slf4j
 public class ClientController {
 
+    private static final MultiValueMap<String, String> POST_HEADERS;
+    static {
+        POST_HEADERS = new HttpHeaders();
+        POST_HEADERS.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+    }
+
     @Value("${provider.goods}")
     private String goods;
 
@@ -36,11 +45,34 @@ public class ClientController {
     }
 
     /**
+     * 添加一条商品信息，使用 restTemplate.exchange() 方法
+     */
+    @PostMapping("/goodsForEntity")
+    public ResultBean<GoodsDTO> goodsForEntity(@RequestBody @Valid GoodsDTO goodsDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) throw new ParamException(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
+        ResponseEntity<ResultBean<GoodsDTO>> goodsForEntity = restTemplate.exchange(goods, HttpMethod.POST, new HttpEntity<>(goodsDTO, POST_HEADERS), new ParameterizedTypeReference<ResultBean<GoodsDTO>>() {});
+        log.info("forEntity:{}", goodsForEntity);
+        if (goodsForEntity.getStatusCode().is2xxSuccessful()) return goodsForEntity.getBody();
+        return new ResultBean<>("添加商品信息失败");
+    }
+
+    /**
      * 获取一条商品信息
      */
     @GetMapping("/goods/{id}")
     public ResultBean<Goods> goods(@PathVariable("id") Long id) {
         return restTemplate.getForObject(goods + Optional.ofNullable(id).orElseThrow(() -> new ParamException("id is required")), ResultBean.class);
+    }
+
+    /**
+     * 获取一条商品信息，使用 restTemplate.exchange() 方法
+     */
+    @GetMapping("/goodsForEntity/{id}")
+    public ResultBean<Goods> goodsForEntity(@PathVariable("id") Long id) {
+        ResponseEntity<ResultBean<Goods>> goodsForEntity = restTemplate.exchange(goods + Optional.ofNullable(id).orElseThrow(() -> new ParamException("id is required")), HttpMethod.GET, new HttpEntity<>(null), new ParameterizedTypeReference<ResultBean<Goods>>() {});
+        log.info("forEntity:{}", goodsForEntity);
+        if (goodsForEntity.getStatusCode().is2xxSuccessful()) return goodsForEntity.getBody();
+        return new ResultBean<>("获取商品信息失败");
     }
 
 }
