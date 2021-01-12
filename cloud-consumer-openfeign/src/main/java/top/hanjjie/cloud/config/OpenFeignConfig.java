1f246f62579dd1541c717bb3abb6824beb021ed5
HANJJIE <1432345840@qq.com>
2021-01-13 00:04:47 +0800

    配置 OpenFeign 日志

--- cloud-consumer-openfeign/src/main/java/top/hanjjie/cloud/config/OpenFeignConfig.java
+++ cloud-consumer-openfeign/src/main/java/top/hanjjie/cloud/config/OpenFeignConfig.java
@@ -1,5 +1,6 @@
 package top.hanjjie.cloud.config;
 
+import feign.Logger;
 import feign.Request;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,15 +12,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenFeignConfig {
 
-    @Value("${openfeign.read-timeout}")
+    @Value("${openfeign.timeout.read}")
     private int connectTimeout;
 
-    @Value("${openfeign.connection-timeout}")
+    @Value("${openfeign.timeout.connection}")
     private int readTimeout;
+
+    @Value("${openfeign.log.level}")
+    private String loggerLevel;
 
     @Bean
     public Request.Options options() {
         return new Request.Options(connectTimeout, readTimeout);
+    }
+
+    @Bean
+    public Logger.Level level() {
+        return Logger.Level.valueOf(loggerLevel);
     }
 
 }
