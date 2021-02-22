ddb35cd9b66168fd3d0ea3bfd05173408429b46f
HANJJIE <1432345840@qq.com>
2021-02-23 00:05:53 +0800

    完成 Config 客户端支持不重启获取最新的远程配置

--- cloud-config-client/src/main/java/top/hanjjie/cloud/controller/ConfigClientController.java
+++ cloud-config-client/src/main/java/top/hanjjie/cloud/controller/ConfigClientController.java
@@ -3,12 +3,14 @@ package top.hanjjie.cloud.controller;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.hanjjie.cloud.utils.ResultBean;
 
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("/cfg")
 public class ConfigClientController {
@@ -16,10 +18,14 @@ public class ConfigClientController {
     @Value("${config.info}")
     private String cfgInfo;
 
+    @Value("${config.version}")
+    private String cfgVersion;
+
     @GetMapping("/info")
     public ResultBean<JSONObject> getCfgInfo() {
-        log.info("返回从 服务配置中 获取的 config.info: {}", cfgInfo);
-        return new ResultBean<>(new JSONObject().fluentPut("cfgInfo", cfgInfo));
+        JSONObject resultConfig = new JSONObject().fluentPut("info", cfgInfo).fluentPut("version", cfgVersion);
+        log.info("返回从 服务配置中 获取的 config info 和 version: {}", resultConfig);
+        return new ResultBean<>(resultConfig);
     }
 
 }
