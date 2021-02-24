c336aeaf727cc5e7aedb0c662ae07c004e0ac4b4
huanghanjie <huanghanjie@badousoft.com>
2021-02-24 15:39:44 +0800

    创建 消息驱动 RabbitMQ Provider 工程，未测试

---
+++ cloud-stream-rmq-provider/src/main/java/top/hanjjie/cloud/StreamRmqProviderApplication.java
@@ -0,0 +1,15 @@
+package top.hanjjie.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class StreamRmqProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRmqProviderApplication.class, args);
+    }
+
+}
