a05ec1a1ad1a81882cffe8499cdbc374929a9963
HANJJIE <1432345840@qq.com>
2021-03-07 22:03:39 +0800

    nacos：服务注册、提供服务、消费服务（轮询），已完成

---
+++ cloud-alibaba-nacos-consumer/src/main/java/top/hanjjie/cloud/controller/NacosConsumerController.java
@@ -0,0 +1,34 @@
+package top.hanjjie.cloud.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import top.hanjjie.cloud.utils.ResultBean;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/nacos")
+public class NacosConsumerController {
+
+    @Value("${provider.nacos}")
+    private String nacosProvider;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public ResultBean<JSONObject> nacosConsumer() {
+        ResponseEntity<ResultBean<JSONObject>> nacosProviderResponse = restTemplate.exchange(nacosProvider + "/nacos/provider", HttpMethod.GET, new HttpEntity<>(null), new ParameterizedTypeReference<ResultBean<JSONObject>>() {});
+        if (nacosProviderResponse.getStatusCode().is2xxSuccessful()) return nacosProviderResponse.getBody();
+        return new ResultBean<>("访问服务提供者失败");
+    }
+
+}
---
+++ cloud-alibaba-nacos-provider/src/main/java/top/hanjjie/cloud/controller/NacosProviderController.java
@@ -0,0 +1,21 @@
+package top.hanjjie.cloud.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.hanjjie.cloud.utils.ResultBean;
+
+@Slf4j
+@RestController
+@RequestMapping("/nacos")
+public class NacosProviderController {
+
+    @GetMapping("/provider")
+    public ResultBean<JSONObject> nacosProvider() {
+        log.info("服务提供接口被调用，返回线程名称");
+        return new ResultBean<>(new JSONObject().fluentPut("Thread name", Thread.currentThread().getName()));
+    }
+
+}
