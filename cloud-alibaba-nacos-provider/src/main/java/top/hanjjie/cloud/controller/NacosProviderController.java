808aeb2d77b8295c612699b0e0abe5f516b8c591
HANJJIE <1432345840@qq.com>
2021-03-07 23:13:46 +0800

    nacos：服务配置，自动刷新配置文件，已完成

--- cloud-alibaba-nacos-provider/src/main/java/top/hanjjie/cloud/controller/NacosProviderController.java
+++ cloud-alibaba-nacos-provider/src/main/java/top/hanjjie/cloud/controller/NacosProviderController.java
@@ -2,12 +2,15 @@ package top.hanjjie.cloud.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.hanjjie.cloud.utils.ResultBean;
 
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("/nacos")
 public class NacosProviderController {
@@ -16,6 +19,15 @@ public class NacosProviderController {
     public ResultBean<JSONObject> nacosProvider() {
         log.info("服务提供接口被调用，返回线程名称");
         return new ResultBean<>(new JSONObject().fluentPut("Thread name", Thread.currentThread().getName()));
+    }
+
+    @Value("${my.name}")
+    private String myName;
+
+    @GetMapping("/config")
+    public ResultBean<JSONObject> nacosConfig() {
+        log.info("测试服务配置");
+        return new ResultBean<>(new JSONObject().fluentPut("My name", myName));
     }
 
 }
